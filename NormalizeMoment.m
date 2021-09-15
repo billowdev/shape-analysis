3f219452f1b5dc0c1766af6d77314d641dbf58ff
Akkarapon Phikulsri <78582854+lacakp@users.noreply.github.com>
2021-09-15 10:55:29 +0700

    Create NormalizeMoment.m

---
+++ NormalizeMoment.m
@@ -0,0 +1,10 @@
+function [mn] = NormalizeMoment(f, m, n)
+	mn=0;
+	j=size(f,1); k=size(f,2);
+	for x=1:size(f,1)
+		for y=1:size(f,2)
+			mn = mn+((x^m)*(y^n)*f(x,y));
+		end
+	end
+mn=mn/(j^m*k^n);
+end
